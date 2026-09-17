a740704cce4ea7537ac84d95f18ae948d776ca4d
dylanturcott <dylanturcott@gmail.com>
2026-09-17 16:13:01 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "docket-cache-v9";
+const CACHE_NAME = "docket-cache-v10";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (event) => {
